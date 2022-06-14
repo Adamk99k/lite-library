cc70818662b262c1f5ac85aac8b19f8ef3f0552a
Adam <Adamkhan0531@hotmail.com>
2022-06-14 11:08:48 +0000

    First func to get user input for what they want to do

--- run.py
+++ run.py
@@ -12,8 +12,36 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lite-library')
 
-books = SHEET.worksheet('books')
 
-data = books.get_all_values()
+def let_user_choose():
+    """
+    Gets user input for what option they want to pick. 
+    either view all books available or borrow a book
+    or return a borrowed book
+    """
+    print("\n")
+    print("Wellcome to Lite Library. \n")
+    print("Please select the service you require. \n")
+    print("1 = View books available.")
+    print("2 = Borrow a book.")
+    print("3 = Return a borrowed book. \n")
 
-print(*data)
+
+    while True:
+        try:
+            user_option = int(input("Enter 1 , 2 or 3 now: "))
+            if user_option > 3:
+                print("No option for that number. Try again")
+                continue
+            elif user_option <= 0:
+                print("Number to low. Try again.")
+                continue
+            print(f'Your choice was {user_option}')
+            break
+        except ValueError:
+            print("Please only choose from 1 / 2 or 3.")
+            continue
+
+
+
+let_user_choose()
