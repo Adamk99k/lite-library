b6cfd92630adb98fabbeca36fb51d7f5de7c7e60
Adam <Adamkhan0531@hotmail.com>
2022-06-14 13:13:31 +0000

    Add basic input validator to validate user input from main menu

--- run.py
+++ run.py
@@ -12,6 +12,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lite-library')
 
+books = SHEET.worksheet('books')
+
+all_books = books.get_all_values()
+
 
 def let_user_choose():
     """
@@ -29,7 +33,8 @@ def let_user_choose():
 
     while True:
         try:
-            user_option = int(input("Enter 1 , 2 or 3 now: "))
+            global user_option
+            user_option = int(input("Enter 1 , 2 or 3 now: \n"))
             if user_option > 3:
                 print("No option for that number. Try again")
                 continue
@@ -41,6 +46,31 @@ def let_user_choose():
         except ValueError:
             print("Please only choose from 1 / 2 or 3.")
             continue
+
+
+    if user_option == 1:
+        print("Viewing all books....\n")
+        for i in all_books:
+            print(i, '\n')
+        print("If you want to borrow a book ")
+        print("please seclect option '2' from the main menu.")
+        print("Make sure you remember the details of the book you want to borrow if thats what you decide to do... \n")
+        repeat = str(input("Want to go to main menu? yes / no : \n")).lower()
+        if repeat == "yes":
+            print("Going to main menu..")
+            let_user_choose()
+        else:
+            print("No problem, Please do come back if you want to use lite library..")
+            quit()
+    elif user_option == 2:
+        print("You want to borrow a book. GREAT!! \n")
+        print("Please follow our instructions to add your borrowed book to our list.")
+        # grab users borrowed book details and add to google sheet 'Borrowed books' then return a message comfiming book has been added successfuly.
+    elif user_option == 3:
+        print("You choose to retunr a book your previously borrowed.")
+        print("Thank you from us at Lite Library for retrning your book.")
+        print("Please follow our prompts so we can update your account on our list to")
+
 
 
 
