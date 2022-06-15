b821439ff560faaff7908718467cd6da093fd853
Adam <Adamkhan0531@hotmail.com>
2022-06-15 17:49:35 +0000

    Added more convenient way by have a var with the text inside then just print the var name when needed

--- run.py
+++ run.py
@@ -16,20 +16,55 @@ books = SHEET.worksheet('books')
 all_books = books.get_all_values()
 
 
+menu_message = """ 
+Wellcome to Lite Library.
+placeholder text
+
+random text
+
+Please select the service you require.
+1 = View books available.
+2 = Borrow a book.
+3 = Return a borrowed book.
+4 = Quit the app.
+"""
+
+borrow_text = """
+Please enter these details they way we show you.
+Each question must be seperated with a ',' 
+Here is what you must write.
+
+Enter your name: David 
+
+Enter your age: 25
+
+Enter your email:  david@gmail.com
+
+Copys of the book are you taking: 1
+
+name of the author: JK Rowling
+
+Title of the book: Harry Potter and the Goblet of Fire
+
+Todays date:  14/5/2021
+
+
+You should type your text like this...
+
+David , 25 , david@gmail.com , 1 , JK Rowling , Harry potter and the Goblet of fire , 14'5'2021
+"""
+
+
 def let_user_choose():
     """
     Gets user input for what option they want to pick. 
     either view all books available or borrow a book
     or return a borrowed book
     """
-    print("\n")
-    print("Wellcome to Lite Library. \n")
-    print("--#placeholder texc--")
-    print("Please select the service you require. \n")
-    print("1 = View books available.")
-    print("2 = Borrow a book.")
-    print("3 = Return a borrowed book.")
-    print("4 = Quit the app. \n")
+
+    print(menu_message)
+
+
     while True:
         try:
             global user_option
@@ -62,9 +97,8 @@ def let_user_choose():
     elif user_option == 2:
         get_data()
     elif user_option == 3:
-        print("You choose to retunr a book your previously borrowed.")
-        print("Thank you from us at Lite Library for retrning your book.")
-        print("Please follow our prompts so we can update your account on our list.")
+        #fucntioon to store the return details
+        pass
     elif user_option == 4:
         print("Goodbye, See you next time! \n")
         quit()
@@ -73,20 +107,14 @@ def let_user_choose():
 # func to grab borrowed books details
 def get_data():
     global borrowed_book
-    print("Please enter these details, Each question must be ")
-    print("seperated with a ',' Here is what you must write.\n")
-    print("Enter your name: David ")
-    print("Enter your age: 25")
-    print("Enter your email:  david@gmail.com")
-    print("Copys of the book are you taking:  1")
-    print("name of the author: JK Rowling")
-    print("Title of the book: Harry Potter and the Goblet of Fire")
-    print("Todays date:  14/5/2021 \n")
-    print("You should type your text like this... \n")
-    print("David , 25 , david@gmail.com , 1 , JK Rowling , Harry potter and the Goblet of fire , 14'5'2021 \n")
+    print(borrow_text)
+
     data_text = input("Please enter your text like the example above now: \n")
     borrowed_book = data_text.split(",")
 
+
+def return_data():
+    print("You have choose the option to return a book. \n")
 
 #def update_worksheet with data user submited for borrowed
 def update_borrowed_book_worksheet(borrowed_book):
