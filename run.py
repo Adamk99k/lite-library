60caf103c0092772598c8af428813c47aac16df5
Adam <Adamkhan0531@hotmail.com>
2022-06-16 06:39:55 +0000

    Added doc-string comment's throughout run.py file and spell check

--- run.py
+++ run.py
@@ -20,17 +20,24 @@ all_books = books.get_all_values()
 
 def let_user_choose():
     """
-    Gets user input for what option they want to pick. 
-    either view all books available or borrow a book
-    or return a borrowed book
+    This function will act as the Main Menu for Lite Library.
+    It will give users a selection of options to take them to
+    the required service.
+    For each option there is a correlated action for python to take.
+    some of which are. 
+    Calling upon other functions to take over.
+    Others are more simple with a default quit() and print() message.
+
+    By keeping all this in one function, it offers a clean way to take 
+    Users back to the Main Menu if needed.
     """
-
+    
     print(menu_message)
-
+    # menu_message is defined on (text.py) file.
 
     while True:
+    # This is a while loop that will keep running until user input is a number between (1-4).
         try:
-            
             user_option = int(input("Enter option now: \n"))
             if user_option >= 5:
                 print("Number to high. Try again")
@@ -42,7 +49,10 @@ def let_user_choose():
         except ValueError:
             print("Please only choose from 1 / 2 / 3 or 4.")
             continue
+
+    
     if user_option == 1:
+        # This will print all books available on the external worksheet, with a delay.
         print("Viewing all books....\n")
         print("We currently have 34 books.\n")
         for i in all_books:
@@ -51,85 +61,68 @@ def let_user_choose():
         print("Want to borrow a book?")
         print("You need to go back to the Main Menu and select that option")
         print("Please keep the details of the book you want to borrow")
-        print("We recommend writing the books details down to use in the borrow book section.\n")
+        print("We recommend writing the book details down to use when prompted to.\n")
        
        
         while True:
+        # User has the option to go back to main menu (restarts). 
+        # Will keep prompting user until a valid response is met.
             try:
                 repeat = str(input("Want to go back to the Main Menu? ( Y = Main Menu / N = Quit App ) \n")).lower()
                 if repeat == "n":
-                    print("Thankyou for viwing the books we have in stock.")
-                    print("We hope next time you find the book your looking for")
+                    print("Thank-you for viewing the books we have in stock.")
+                    print("We hope next time you find the book your looking for.")
                     for i in range(3):
-                        print("App quiting...")
+                        print("App quitting...")
                         time.sleep(1)
                     print("App quit successfuly.")
                     quit()
                 elif repeat == "y":
-                    print("Going back to main menu.")
+                    print("Going back to Main Menu.\n")
                     let_user_choose()
                 else:
                     print("Try again.")
                     continue
             except ValueError:
                 pass
-            
-
-
     elif user_option == 2:
         get_data()
     elif user_option == 3:
         return_data()
     elif user_option == 4:
         for i in range(3):
-            print("App quiting...")
+            print("App quitting...")
             time.sleep(1)
-        print("App quit successfuly.")
+        print("App quit successfully.")
         quit()
 
 
-# function to grab data
-def get_data():
-    global borrowed_book
-
-    print(borrow_text)
-
-    data_text = input("Please enter your text like the example above now: \n")
-    borrowed_book = data_text.split(",")
-    update_borrowed_book_worksheet(borrowed_book)
-
-# function to grab details about the returned book to use in update function
-def return_data():
-    global returned_book
-
-    print(return_message)
-
-    return_text = input("Please enter your text like the example above now: \n")
-    returned_book = return_text.split(",")
-    update_returned_book_worksheet(returned_book)
-
-
-
-
-#def update_worksheet with data user submited for borrowed
 def update_borrowed_book_worksheet(borrowed_book):
     """
-    Takes the taken data for borrowed book and adds to worksheet
+    This function will append the data passed to it to the (borrowed-books) worksheet
+    and a delayed print message to keep the user updated on what's happening.
+
+    This function takes one variable named (borrowed_book). This would have already been defined 
+    as this function only gets run after (borrowed_book) is defined.
     """
     for i in range(3):
-        print("Updateing worksheet... \n")
+        print("Updating worksheet... \n")
         time.sleep(0.5)
     
     borrowed_worksheet = SHEET.worksheet("borrowed-books")
     borrowed_worksheet.append_row(borrowed_book)
-    print("Updated sucsesfully...")
-    print("Going back to main menu.")
+    print("Updated successfully...")
+    print("Going back to Main Menu.\n")
     let_user_choose()
 
-#function to update returned-books sheet useing the data from return_data function.
+
 def update_returned_book_worksheet(returned_book):
     """
-    #
+    This function will append the data passed to it to the (returned-books) worksheet
+    and a delayed print message to keep the user updated on what's happening.
+
+    This function takes one variable named (returned_book). 
+    The function only gets run after returned_book variable has been defined.
     """
     return_worksheet = SHEET.worksheet("returned-books")
     return_worksheet.append_row(returned_book)
@@ -137,7 +130,34 @@ def update_returned_book_worksheet(returned_book):
         print("Updating..")
         time.sleep(1)
     print("Update complete.\n")
-    print("Going back to main menu.")
+    print("Going back to Main Menu. \n")
     let_user_choose()
+
+
+def get_data():
+    """
+    When called, this function will grab the details needed for (borrowed-books) worksheet, Split them up
+    and store them in a new variable. These details can be given to a function to append them onto the worksheet.
+    """
+    global borrowed_book
+    print(borrow_text)
+    data_text = input("Please enter your text like the example above now: \n")
+    borrowed_book = data_text.split(",")
+    # Calling the append() function and passing it the new variable made.
+    update_borrowed_book_worksheet(borrowed_book)
+
+
+def return_data():
+    """
+    When called, this function will grab the return details needed, Split them up and store in a new variable.
+    """
+    global returned_book
+    print(return_message)
+    return_text = input("Please enter your text like the example above now: \n")
+    returned_book = return_text.split(",")
+
+    # Calling append() fucntion to add detials to returned worksheet.
+    update_returned_book_worksheet(returned_book)
+
 
 let_user_choose()
