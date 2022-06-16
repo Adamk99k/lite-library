029bc0114cd0eb8dfef0e74199aa421a88ac2cac
Adam <Adamkhan0531@hotmail.com>
2022-06-16 06:47:57 +0000

    Spell check for text.py file

--- text.py
+++ text.py
@@ -1,10 +1,10 @@
 menu_message = """ 
-Wellcome to Lite Library.
+Welcome to Lite Library.
 
 Here at Lite Library we only keep top rated books.
 This is our automated system to keep track and log
-what books we have, and other infomation.
-We value your service and we wish you have a good
+what books we have, and other information.
+We value your service and wish you have a good
 experience.
 
 Please type the service you require.
@@ -16,52 +16,53 @@ Please type the service you require.
 """
 
 borrow_text = """
-Hey and thanks for chooseing to borrow a book.
+Hey and thanks for choosing to borrow a book.
 Please answer these questions in the format we show.
-Each question must be seperated with a ',' 
+Each question must be separated with a ',' comma.
 Here is what you must write.
 
-1,Enter your name: David 
+1,   Enter your name: David 
 
-2,Enter your age: 25
+2,   Enter your age: 25
 
-3,Enter your email:  david@gmail.com
+3,   Enter your email:  david@gmail.com
 
-4,Copys of the book are you taking: 1
+4,   Copys of the book are you taking: 1
 
-5,Name of the author: JK Rowling
+5,   Name of the author: JK Rowling
 
-6,Title of the book: Harry Potter and the Goblet of Fire
+6,   Title of the book: Harry Potter and the Goblet of Fire
 
-7,Todays date:  14/5/2021
+7,   Todays date:  14/5/2021
 
 You should write your text like this.
 David , 25 , david@gmail.com , 1 , JK Rowling , Harry potter and the Goblet of fire , 14/5/2021
 """
 
 return_message = """
-Wellcome to the return book page
-Lets get started...
+Welcome to the return book page
+Let'ss get started...
 Please answer these questions in the format we show.
-Each question must be seperated with a ',' 
+Each question must be separated with a ',' comma.
 Here is what you must write.
 
-1,Enter your name: David 
+1,   Enter your name: David 
 
-2,Enter your age: 25
+2,   Enter your age: 25
 
-3,Enter your email:  david@gmail.com
+3,   Enter your email:  david@gmail.com
 
-4,Copys of the book are you returning: 1
+4,   Copys of the book are you returning: 1
 
-5,Name of the author: JK Rowling
+5,   Name of the author: JK Rowling
 
-6,Title of the book: Harry Potter and the Goblet of Fire
+6,   Title of the book: Harry Potter and the Goblet of Fire
 
-7,Date of when you took the book:  14/5/2021
+7,   Date of when you took the book:  14/5/2021
 
-8,Todays date: 13/6/2021
+8,   Todays date: 13/6/2021
 
 You should write your text like this.
 David , 25 , david@gmail.com , 1 , JK Rowling , Harry potter and the Goblet of fire , 14/5/2021 , 13/6/2021
+
 """
